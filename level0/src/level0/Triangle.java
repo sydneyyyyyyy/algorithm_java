9afa1b3228d0c4ad895e1cf95f47ff1f4116cad3
sydneyyyyyyy <qwer1478792@gmail.com>
2023-01-30 21:48:06 +0900

    [programmers] level0.삼각형조건1

---
+++ level0/src/level0/Triangle.java
@@ -0,0 +1,42 @@
+package level0;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		int[] sides = {1,2,3};
+		int answer = 0;
+		
+		//최댓값구하기
+		int max = sides[0];
+		
+		for(int i = 0; i<sides.length; i++) {
+			if(max < sides[i]) {
+				max = sides[i];
+			}
+		}
+		
+		//총합
+		int sum = 0;
+		for (int s:sides) {
+			sum+=s;
+		}
+		
+		if(max <sum - max) {
+			answer = 1;
+		} else {
+			answer = 2;
+		}
+		
+		System.out.print(answer);
+	}
+
+}
+
+/*
+Arrays 의 sort 메서드를 이용해 정렬한 후 인덱스 번호로만 구함
+ 
+  Arrays.sort(sides);
+  return sides[2] >= sides[0]+sides[1] ? 2 : 1;
+ 
+ 
+ */
