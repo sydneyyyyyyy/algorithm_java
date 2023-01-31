4d7d944db114e3dd86d2ba6bd4d608515363d043
sydneyyyyyyy <110385417+sydneyyyyyyy@users.noreply.github.com>
2023-01-31 09:28:13 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		
+		for(int i=1;i<=n;i+=2) {
+			arr.add(i);
+		}
+		int[] answer = new int[arr.size()];
+		for(int i=0; i<answer.length;i++) {
+			answer[i] = arr.get(i);
+		}
+        return answer;
+    }
+    
+}
